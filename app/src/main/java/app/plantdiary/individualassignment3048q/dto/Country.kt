904c344bdb0fdfed304d69cfeae75acdd7d8900c
rankeyme <markheelies@yahoo.com>
2021-02-28 16:05:19 -0500

    gson annotation

--- app/src/main/java/app/plantdiary/individualassignment3048q/dto/Country.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/dto/Country.kt
@@ -2,7 +2,7 @@ package app.plantdiary.individualassignment3048q.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class Country(var names: String, var codes: String) {
+data class Country(var names: String, @SerializedName("codes") var codes: String) {
     public final var code = names
     public final var name = codes
     override fun toString(): String {
